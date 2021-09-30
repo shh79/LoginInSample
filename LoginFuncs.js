a67a5494a08b03bb3f80a219f245a8c1bbf8fdfd
shh79 <shh79@gmail.com>
2021-09-30 14:50:58 +0330

    adding server side functions

--- LoginFuncs.js
+++ LoginFuncs.js
@@ -15,9 +15,39 @@ function OpenContainer(type){
     }
 }
 
+async function SignInFunc(username, pass){
+    let adderss = `/SignIn/:${username},${pass}`;
+    await fetch(adderss, {
+        method: 'POST',
+    });
+}
+
+async function SignUpFunc(username, pass, rePass){
+    let adderss = `/SignUp/:${username},${pass},${rePass}`;
+    await fetch(adderss, {
+        method: 'POST',
+    });
+}
+
 document.querySelector("#SignInBtn").addEventListener('click', () => {
     OpenContainer('in');
 });
+
 document.querySelector("#SignUpBtn").addEventListener('click', () => {
     OpenContainer('up');
+});
+
+document.querySelector("#green").addEventListener('click', () => {
+    let username = document.querySelector("#UserIn").value;
+    let pass = document.querySelector("#PassIn").value;
+
+    SignInFunc(username, pass);
+});
+
+document.querySelector("#red").addEventListener('click', () => {
+    let username = document.querySelector("#UserUp").value;
+    let pass = document.querySelector("#PassUp").value;
+    let rePass = document.querySelector("#RePassUp").value;
+
+    SignUpFunc(username, pass, rePass);
 });
